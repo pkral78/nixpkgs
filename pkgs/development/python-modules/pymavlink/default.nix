81619f02fd35e3ed9c47215b296f7b8e0dc5e05b
Frederik Rietdijk <fridh@fridh.nl>
2019-12-19 20:31:19 +0100

    python: pymavlink: 2.4.1 -> 2.4.2

--- pkgs/development/python-modules/pymavlink/default.nix
+++ pkgs/development/python-modules/pymavlink/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "pymavlink";
-  version = "2.4.1";
+  version = "2.4.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0y9rz3piddzdjpp7d5w9xi6lc9v9b4p4375a5hrfiphrmhds85i3";
+    sha256 = "80ee6481cf9cb566d7546786fd4401fdb575462645a5349efa7737cacdb0b86f";
   };
 
   propagatedBuildInputs = [ future lxml ];
