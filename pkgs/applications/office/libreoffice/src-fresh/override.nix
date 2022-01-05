cdba842bb4c100bcad92c7a719d4546158736b91
Pavel Král <pavel.kral@gmail.com>
2022-01-05 13:59:18 +0100

    libreoffice-fresh: fix build #153520

--- pkgs/applications/office/libreoffice/src-fresh/override.nix
+++ pkgs/applications/office/libreoffice/src-fresh/override.nix
@@ -1,4 +1,4 @@
-{ lib, kdeIntegration, ... }:
+{ lib, kdeIntegration, fetchpatch, ... }:
 attrs:
 {
   postConfigure = attrs.postConfigure + ''
@@ -7,5 +7,19 @@ attrs:
   configureFlags = attrs.configureFlags ++ [
     (lib.enableFeature kdeIntegration "kf5")
     "--without-system-zxing"
+  ];
+  patches = attrs.patches or [ ] ++ [
+    # CppunitTest_sc_subsequent_filters_test2: fix test depending on current year
+    # https://gerrit.libreoffice.org/c/core/+/127361
+    (fetchpatch {
+      url = "https://github.com/LibreOffice/core/commit/4f448763825f4e906d221d9e1059ea344d7a1ad1.patch";
+      sha256 = "sha256-3fgxcgGDMp/zbZ+0nAlZC+skZZmBbU7O74G5aPV67M0=";
+    })
+    # CppunitTest_sc_subsequent_filters_test2: check format instead of string
+    # https://gerrit.libreoffice.org/c/core/+/127887
+    (fetchpatch {
+      url = "https://github.com/LibreOffice/core/commit/d5f36648643399ad63006a97a1f58b81690ccddf.patch";
+      sha256 = "sha256-IOvY92jYxF8kH3IrNL5FlUSLAF04XOBglVhsF8I+/EU=";
+    })
   ];
 }
